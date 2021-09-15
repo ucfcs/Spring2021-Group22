8d34d1cda761dbcce0d5359d8d0e85c6f92cb18c
Ryan <ryan.cfplus@gmail.com>
2021-09-14 23:02:02 -0400

    Add website to repo

---
+++ Website/tools/generate_inventory_size_over_time_time_series.py
@@ -0,0 +1,65 @@
+## Simple tool for querying the database for common events
+
+import pymongo
+import argparse
+from dotenv import load_dotenv
+import os
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import json
+
+load_dotenv();
+
+MONGO_URI = 'MONGO_URI';
+if (MONGO_URI not in os.environ.keys()):
+    print('Please add MONGO_URI to your environment variables before using this utility');
+    exit(0);
+
+mongo_connection_uri = os.environ.get(MONGO_URI);
+
+# I've changed the schema so without this the script crashes. This check will
+# eventually be unnecessary
+def isGoodData(event):
+    return 'totalSize' in event;
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--out', help='where to write the data to');
+parser.add_argument('--experiment', help='the experiment label to limit the data to');
+args = parser.parse_args()
+
+# Precompute the data structure needed for the inventory size time series. This would be
+# the "tools" part of the process. This would be run once for each team we have run
+# through the map
+def precomputeJSON(experimentLabel):
+    client = pymongo.MongoClient(mongo_connection_uri, serverSelectionTimeoutMS=5000)
+    collection = client.epilog.data2;
+    query = { "event": "InventoryContent" }
+    if experimentLabel != None:
+        query['experimentLabel'] = experimentLabel
+    cursor = collection.find(query, sort=[('time', pymongo.ASCENDING)])
+    data = {};
+    for event in cursor:
+        if not isGoodData(event):
+            continue
+        if event['player'] not in data:
+            data[event['player']] = {'x': [], 'y': []}
+        data[event['player']]['x'].append(event['time'] // 1000);
+        data[event['player']]['y'].append(event['totalSize']);
+    return data
+
+# Simulate the client. THIS IS A SERVER SIDE MOCK. THE ACTUAL VISUALIZATIONS
+# WILL BE JS.
+def generatePlot(data):
+    plt.title('Inventory Sizes Over Time')
+    for player in data:
+        x = [datetime.fromtimestamp(x) for x in data[player]['x']]
+        y = data[player]['y'];
+        plt.plot(x, y);
+    plt.show();
+
+def writeToFile(data, file):
+    with open(file, 'w+') as out:
+        json.dump(data, out)
+
+data = precomputeJSON(args.experiment)
+writeToFile(data, args.out if args.out != None else '../static_files/inventory_size_over_time.json')
