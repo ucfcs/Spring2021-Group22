4b7daf042e1d666c0956b9fbb47e66d98f5c3c09
jkraftchick <54610324+jkraftchick@users.noreply.github.com>
2021-11-05 22:24:42 -0400

    move script into html

--- Heatmap/heatmap.js
+++ Heatmap/heatmap.js
@@ -1,52 +1,154 @@
-// set the dimensions and margins of the graph
-const margin = { top: 10, right: 30, bottom: 30, left: 40 },
-	width = 460 - margin.left - margin.right,
-	height = 400 - margin.top - margin.bottom;
+async function heatmap() {
+	const SCALE = 2
+	let width = 417 * SCALE, height = 417 * SCALE; // 417 is image size
+	const CENTER = { x: width / 2, y: height / 2 };
 
-// append the svg object to the body of the page
-const svg = d3.select("#my_dataviz")
-	.append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	.attr("transform", `translate(${margin.left},${margin.top})`);
+	function coord(x, y) {
+		return [Math.floor(x * SCALE + CENTER.x), Math.floor(y * SCALE + CENTER.y)]
+	}
 
-// read data
-d3.csv("https://raw.githubusercontent.com/ucfcs/Spring2021-Group22/0e9b8c2982c27f3dd56762a320472cf4de18a080/Heatmap/rawdata_timestamp.csv").then(function (data) {
+	// https://css-tricks.com/converting-color-spaces-in-javascript/
+	function rgb2hex(rgb) {
+		r = rgb[0].toString(16);
+		g = rgb[1].toString(16);
+		b = rgb[2].toString(16);
 
-	// Add X axis
-	const x = d3.scaleLinear()
-		.domain([5, 20])
-		.range([margin.left, width - margin.right]);
-	svg.append("g")
-		.attr("transform", `translate(0, ${height})`)
-		.call(d3.axisBottom(x));
+		if (r.length == 1)
+			r = "0" + r;
+		if (g.length == 1)
+			g = "0" + g;
+		if (b.length == 1)
+			b = "0" + b;
 
-	// Add Y axis
-	const y = d3.scaleLinear()
-		.domain([5, 25])
-		.range([height - margin.bottom, margin.top]);
-	svg.append("g")
-		.call(d3.axisLeft(y));
+		return "#" + r + g + b;
+	}
 
-	// Prepare a color palette
-	const color = d3.scaleLinear()
-		.domain([0, 1]) // Points per square pixel.
-		.range(["white", "#69b3a2"])
+	function scale(number, inMin, inMax, outMin, outMax) {
+		return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
 
-	// compute the density data
-	const densityData = d3.contourDensity()
-		.x(function (d) { return x(d.x); })
-		.y(function (d) { return y(d.y); })
-		.size([width, height])
-		.bandwidth(20)
-		(data)
 
-	// show the shape!
-	svg.insert("g", "g")
-		.selectAll("path")
-		.data(densityData)
-		.enter().append("path")
-		.attr("d", d3.geoPath())
-		.attr("fill", function (d) { return color(d.value); })
-})
+	// https://stackoverflow.com/questions/17242144/javascript-convert-hsb-hsv-color-to-rgb-accurately
+	function hsv2rgb(h, s, v) {
+		var r, g, b, i, f, p, q, t;
+		i = Math.floor(h * 6);
+		f = h * 6 - i;
+		p = v * (1 - s);
+		q = v * (1 - f * s);
+		t = v * (1 - (1 - f) * s);
+		switch (i % 6) {
+			case 0: r = v, g = t, b = p; break;
+			case 1: r = q, g = v, b = p; break;
+			case 2: r = p, g = v, b = t; break;
+			case 3: r = p, g = q, b = v; break;
+			case 4: r = t, g = p, b = v; break;
+			case 5: r = v, g = p, b = q; break;
+		}
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
+	}
+
+
+
+	// Select map div and set attributes
+	let map = d3.select("div#my_dataviz")
+		.style("width", width + "px")
+		.style("height", height + "px")
+
+	// create base svg
+	let _svg = map
+		.append("svg")
+		.style("width", width + "px")
+		.style("height", height + "px")
+	let svg = _svg.append("g")
+
+	// add image to svg
+	let image = svg.append("image")
+		.attr("width", width + "px")
+		.attr("height", height + "px")
+		.attr("xlink:href", "highresmap.png")
+
+	// Create square that is able to be placed under selected cell as outline
+	// weird issues trying to use css outline on elements that are only a few pixels in size
+	let selected = svg.append("rect")
+		.attr("x", '0')
+		.attr("y", '0')
+		.attr("width", `${SCALE * 2}px`)
+		.attr("height", `${SCALE * 2}px`)
+		.style("opacity", 0)
+
+
+	//Read the data
+	// d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv").then(function (data) {
+	d3.csv('https://raw.githubusercontent.com/ucfcs/Spring2021-Group22/78fe6d609d3de713462bd984d3cdcfb7ae9999b5/Heatmap/heatmap.csv').then(function (data) {
+
+		let max = Math.max(...data.map(d => parseInt(d.count)))
+
+		// create a tooltip
+		const tooltip = d3.select("#tooltip")
+			.append("div")
+			.style("opacity", 0)
+			.attr("class", "tooltip")
+			.style("background-color", "white")
+			.style("border", "solid")
+			.style("border-width", "2px")
+			.style("border-radius", "5px")
+			.style("padding", "5px")
+
+		// Three function that change the tooltip when user hover / move / leave a cell
+		const mouseover = function (event, d) {
+			selected
+				.attr("x", function () { return coord(d.x, 0)[0] - 0.5 - (SCALE / 2) })
+				.attr("y", function () { return coord(0, d.y)[1] - 0.5 - (SCALE / 2) })
+				.style("opacity", 1)
+				.raise()
+			tooltip
+				.style("opacity", 1)
+			d3.select(this)
+				.style("opacity", 1)
+				.raise()
+		}
+		const mousemove = function (event, d) {
+			tooltip
+				.html(`The exact value of<br>this cell is: ${d.count} at ${d.x}, ${d.y}`)
+				.style("left", (event.x) / 2 + "px")
+				.style("top", (event.y) / 2 + "px")
+		}
+		const mouseleave = function (event, d) {
+			tooltip
+				.style("opacity", 0)
+			d3.select(this)
+				.style("opacity", 0.8)
+			selected.style("opacity", 0)
+
+		}
+
+		// add the squares
+		svg.selectAll()
+			.data(data, function (d) { return d.x + ':' + d.y; })
+			.join("rect")
+			.attr("x", function (d) { return coord(d.x, 0)[0] - 0.5 })
+			.attr("y", function (d) { return coord(0, d.y)[1] - 0.5 })
+			.attr("width", `${SCALE}px`)
+			.attr("height", `${SCALE}px`)
+			.style("fill", function (d) { return rgb2hex(hsv2rgb(scale(d.count, 0, max, 0.2, 0.8), 0.8, 0.8)) })
+			.style("opacity", 0.8)
+			.on("mouseover", mouseover)
+			.on("mousemove", mousemove)
+			.on("mouseleave", mouseleave)
+	})
+
+	let zoom = d3.zoom()
+		.translateExtent([[0, 0], [width, height]])
+		.scaleExtent([1, 5])
+		.on('zoom', handleZoom);
+
+	function handleZoom(e) {
+		svg.attr('transform', e.transform);
+	}
+
+	function initZoom() {
+		svg.call(zoom)
+	}
+
+	initZoom();
+}
